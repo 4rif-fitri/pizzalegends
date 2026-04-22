2da903d2836de0779d377b35dd876d0163288b13
Arif <dopymonster@gmail.com>
2026-04-23 01:52:51 +0800

    add shadow

--- OverWorld.js
+++ OverWorld.js
@@ -12,10 +12,41 @@ class OverWorld{
 		}
 		image.src = "./images/maps/DemoLower.png"
 
+		let x = 5
+		let y = 6
+
+		let shadow = new Image()
+		shadow.onload = () => {
+			this.ctx.drawImage(
+				shadow,
+				0,
+				0,
+				32,
+				32,
+				x * 16 - 8,
+				y * 16 - 18,
+				32,
+				32	
+			)
+		}
+		shadow.src = "./images/characters/shadow.png" 
+
 		let hero = new Image()
 		hero.onload = () => {
-			this.ctx.drawImage(hero,0,0)
+			this.ctx.drawImage(
+			hero,
+			0, //left cut
+			0, //top cut
+			32,//width cut 
+			32,//heigt cut
+			x * 16 - 8, //poss X
+			y * 16 - 18,//poss Y
+			32, //width
+			32 
+			)
 		}
 		hero.src = "./images/characters/people/hero.png"
+
+
 	}
 }
